b53f3e0087a23bea6f199987f5f0c0dcebc074ee
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2024-11-18 23:04:18 +0530

    all

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-function blowOutCandles() {
-    const flames = document.querySelectorAll('.flame');
-    
-    // Blow out the candles one by one with a delay
-    let delay = 0;
-    flames.forEach((flame, index) => {
+window.onload = function blowOutCandles() {
+  const flames = document.querySelectorAll('.flame');
+  
+  // Blow out the candles one by one with a delay
+  let delay = 0;
+  flames.forEach((flame, index) => {
       setTimeout(() => {
-        flame.style.animation = 'none';
-        flame.style.opacity = '0';
+          flame.style.animation = 'none';
+          flame.style.opacity = '0';
       }, delay);
       delay += 1000; // Add 1-second delay for each candle
-    });
-  
-    // After 5 seconds, trigger confetti and redirect
-    setTimeout(() => {
+  });
+
+  // After 5 seconds, trigger confetti and redirect
+  setTimeout(() => {
       // Redirect to confetti.html after 5 seconds
       window.location.href = 'confetti.html';
-    }, 5000); // 5000ms = 5 seconds
-  }
-  
+  }, 8000); // 5000ms = 5 seconds
+}
